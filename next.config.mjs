62f56f8e8e1c0a9dfa960a72618a1d5f38218348
Jason Velarde <gotnovalues@gmail.com>
2024-12-07 01:36:18 -0500

    standalone output

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,8 @@ const nextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  }
+  },
+  output: 'standalone'
 };
 
 export default nextConfig;
